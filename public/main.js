d23eaf08f8a34b29f8e056c40f9ecee2860c1ec3
Erin Soggs <erinasoggs@gmail.com>
2020-07-02 14:25:33 -0400

    looped through all restaurants to create pins on map for each

--- public/main.js
+++ public/main.js
@@ -1,5 +1,3 @@
-console.log('good to go')
-
 let myMap = L.map('map').setView([44.4798, -73.2143], 12)
 
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
@@ -24,6 +22,7 @@ marker.addEventListener('mouseover', () => {
   marker.openPopup()
 })
 
+
 function placeMarker(address) {
   let urlAddress = encodeURI(address)
   fetch(`https://nominatim.openstreetmap.org/search?q=${urlAddress}&format=json`)
@@ -32,24 +31,36 @@ function placeMarker(address) {
     let latLongArr = [json[0].lat, json[0].lon]
     L.marker(latLongArr).addTo(myMap)
   })
-
+  
 }
 
-placeMarker('163 Main St. Burlington, VT 05401')
-placeMarker('115 St Paul St. Burlington, VT 05401')
-placeMarker('149 S Champlain St. Burlington, VT 05401')
-placeMarker('82 S Winooski Ave. Burlington, VT 05401')
-placeMarker('189 Bank St. Burlington, VT 05401')
-placeMarker('160 Bank St. Burlington, VT 05401')
-placeMarker('144 Church St. Burlington Vermont 05401')
-placeMarker('55 Cherry St. Burlington, VT 05401')
-placeMarker('155 Bank St. Burlington Vermont 05401')
-placeMarker('156 Church St. Burlington Vermont 05401')
-placeMarker('155 Main St. Burlington, VT 05401')
-placeMarker('115 Church St. Burlington Vermont 05401')
-placeMarker('206 Main St. Burlington, VT 05401')
-placeMarker('83 Church St. Burlington, VT 05401')
-placeMarker('112 Lake St. Burlington Vermont 05401')
-placeMarker('161 Church St. Burlington Vermont 05401')
+function getRestaurants() {
+  fetch('https://json-server.burlingtoncodeacademy.now.sh/restaurants')
+  .then((res) => res.json())
+  .then(restaurants => {
+    restaurants.forEach(restaurant => {
+      placeMarker(restaurant.address)
+    })
+  })
+}
+
+getRestaurants()
+
+// placeMarker('163 Main St. Burlington, VT 05401')
+// placeMarker('115 St Paul St. Burlington, VT 05401')
+// placeMarker('149 S Champlain St. Burlington, VT 05401')
+// placeMarker('82 S Winooski Ave. Burlington, VT 05401')
+// placeMarker('189 Bank St. Burlington, VT 05401')
+// placeMarker('160 Bank St. Burlington, VT 05401')
+// placeMarker('144 Church St. Burlington Vermont 05401')
+// placeMarker('55 Cherry St. Burlington, VT 05401')
+// placeMarker('155 Bank St. Burlington Vermont 05401')
+// placeMarker('156 Church St. Burlington Vermont 05401')
+// placeMarker('155 Main St. Burlington, VT 05401')
+// placeMarker('115 Church St. Burlington Vermont 05401')
+// placeMarker('206 Main St. Burlington, VT 05401')
+// placeMarker('83 Church St. Burlington, VT 05401')
+// placeMarker('112 Lake St. Burlington Vermont 05401')
+// placeMarker('161 Church St. Burlington Vermont 05401')
 
 
