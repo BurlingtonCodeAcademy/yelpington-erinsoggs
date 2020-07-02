d0a19c8a76faa4da4b2b886c5fc9603e86adeee6
Erin Soggs <erinasoggs@gmail.com>
2020-07-01 20:53:20 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const app = express()
+const got = require('got')
+const path = require('path')
+
+
+const port = process.env.PORT || 5000
+const public = path.resolve('./public')
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(public + '/index.html')
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ public/main.js
@@ -0,0 +1,55 @@
+console.log('good to go')
+
+let myMap = L.map('map').setView([44.4798, -73.2143], 12)
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  maxZoom: 17,
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(myMap)
+
+L.marker([44.4798, -73.2143]).addTo(myMap);
+
+L.circle([44.4798, -73.2143], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(myMap);
+
+let marker = L.marker([44.4798, -73.2143])
+marker.bindPopup('<h4>Downtown Burlington</h4>')
+marker.addTo(myMap)
+
+marker.addEventListener('mouseover', () => {
+  marker.openPopup()
+})
+
+function placeMarker(address) {
+  let urlAddress = encodeURI(address)
+  fetch(`https://nominatim.openstreetmap.org/search?q=${urlAddress}&format=json`)
+  .then((res) => res.json())
+  .then (json => {
+    let latLongArr = [json[0].lat, json[0].lon]
+    L.marker(latLongArr).addTo(myMap)
+  })
+
+}
+
+placeMarker('163 Main St. Burlington, VT 05401')
+placeMarker('115 St Paul St. Burlington, VT 05401')
+placeMarker('149 S Champlain St. Burlington, VT 05401')
+placeMarker('82 S Winooski Ave. Burlington, VT 05401')
+placeMarker('189 Bank St. Burlington, VT 05401')
+placeMarker('160 Bank St. Burlington, VT 05401')
+placeMarker('144 Church St. Burlington Vermont 05401')
+placeMarker('55 Cherry St. Burlington, VT 05401')
+placeMarker('155 Bank St. Burlington Vermont 05401')
+placeMarker('156 Church St. Burlington Vermont 05401')
+placeMarker('155 Main St. Burlington, VT 05401')
+placeMarker('115 Church St. Burlington Vermont 05401')
+placeMarker('206 Main St. Burlington, VT 05401')
+placeMarker('83 Church St. Burlington, VT 05401')
+placeMarker('112 Lake St. Burlington Vermont 05401')
+placeMarker('161 Church St. Burlington Vermont 05401')
+
+
