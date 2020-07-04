b405c5a679d8259fbca908c6c182d633b5271be2
Erin Soggs <erinasoggs@gmail.com>
2020-07-04 13:38:09 -0400

    getting mad, need break

--- public/main.js
+++ public/main.js
@@ -7,12 +7,12 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 
 L.marker([44.4798, -73.2143]).addTo(myMap);
 
-L.circle([44.4798, -73.2143], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 500
-}).addTo(myMap);
+// L.circle([44.4798, -73.2143], {
+//   color: 'red',
+//   fillColor: '#f03',
+//   fillOpacity: 0.5,
+//   radius: 500
+// }).addTo(myMap);
 
 let marker = L.marker([44.4798, -73.2143])
 marker.bindPopup('<h4>Downtown Burlington</h4>')
@@ -34,13 +34,13 @@ function placeMarker(address) {
   
 }
 
-fetch("https://json-server.burlingtoncodeacademy.now.sh/restaurants")
-.then((res) => res.json())
-.then((restaurantCollection) => {
-  restaurantCollection.forEach((restaurant) => {
-    placeMarker(restaurant.address)
-  })
-})
+// fetch("https://json-server.burlingtoncodeacademy.now.sh/restaurants")
+// .then((res) => res.json())
+// .then((restaurantCollection) => {
+//   restaurantCollection.forEach((restaurant) => {
+//     placeMarker(restaurant.address)
+//   })
+// })
 
 let display = document.getElementById("display")
 
@@ -53,24 +53,5 @@ fetch('https://json-server.burlingtoncodeacademy.now.sh/restaurants')
     display.appendChild(item)
   })
 })
-
-
-
-// placeMarker('163 Main St. Burlington, VT 05401')
-// placeMarker('115 St Paul St. Burlington, VT 05401')
-// placeMarker('149 S Champlain St. Burlington, VT 05401')
-// placeMarker('82 S Winooski Ave. Burlington, VT 05401')
-// placeMarker('189 Bank St. Burlington, VT 05401')
-// placeMarker('160 Bank St. Burlington, VT 05401')
-// placeMarker('144 Church St. Burlington Vermont 05401')
-// placeMarker('55 Cherry St. Burlington, VT 05401')
-// placeMarker('155 Bank St. Burlington Vermont 05401')
-// placeMarker('156 Church St. Burlington Vermont 05401')
-// placeMarker('155 Main St. Burlington, VT 05401')
-// placeMarker('115 Church St. Burlington Vermont 05401')
-// placeMarker('206 Main St. Burlington, VT 05401')
-// placeMarker('83 Church St. Burlington, VT 05401')
-// placeMarker('112 Lake St. Burlington Vermont 05401')
-// placeMarker('161 Church St. Burlington Vermont 05401')
 
 
--- public/rest-page.js
+++ public/rest-page.js
@@ -31,6 +31,55 @@ let notes = restaurant.notes
     restHours.textContent = hours
   })
 
-  let myMap = L.map('mapid').setView([51.505, -0.09], 13)
+  // MAP
+
+  let myMap = L.map("restMap").setView([44.4798, -73.2143], 12)
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  maxZoom: 17,
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(myMap)
+
+// L.marker([44.4798, -73.2143]).addTo(myMap);
+
+// L.circle([44.4798, -73.2143], {
+//   color: 'red',
+//   fillColor: '#f03',
+//   fillOpacity: 0.5,
+//   radius: 500
+// }).addTo(myMap);
+
+// let marker = L.marker([44.4798, -73.2143])
+// marker.bindPopup('<h4>Downtown Burlington</h4>')
+// marker.addTo(myMap)
+
+// marker.addEventListener('mouseover', () => {
+//   marker.openPopup()
+// })
+
+
+function placeMarker(address) {
+  let urlAddress = encodeURI(address)
+  fetch(`https://nominatim.openstreetmap.org/search?q=${urlAddress}&format=json`)
+  .then((res) => res.json())
+  .then (json => {
+    let latLongArr = [json[0].lat, json[0].lon]
+    L.marker(latLongArr).addTo(myMap)
+  })
+}
+
+fetch("https://json-server.burlingtoncodeacademy.now.sh/restaurants")
+.then((res) => res.json())
+.then((restaurantCollection) => {
+  for(let restaurant in restaurantCollection) {
+    placeMarker(restaurant.address)
+
+  }
+  
+  })
+
+
+
+
 
 
